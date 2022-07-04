79855397e84c9c0fa3aaf961bd9e4caa0a2ae728
dương đặng tuấn <july30dtd@gmail,com>
2022-07-04 13:06:57 +0700

    update menu 2

--- js/main.js
+++ js/main.js
@@ -3,23 +3,14 @@ var closeBtn = document.querySelector('.close-btn');
 var navbarWrapper = document.querySelector('.hal-nav');
 var navbarShow = false;
 var overlay = document.querySelector('.overlay');
-menuBtn.addEventListener('click', function() {
-    navbarShow = !navbarShow;
-    if(navbarShow === true) {
-        navbarWrapper.classList.remove('hide-to-left');
-        overlay.style.visibility = 'visible';
-    }
-    else {
-        navbarWrapper.classList.add('hide-to-left');
-        overlay.style.visibility = 'hidden';
-    }
+menuBtn.addEventListener('click', function () {
+    navbarWrapper.classList.remove('hide-to-left');
+    overlay.classList.add('show');
 })
 
-closeBtn.addEventListener('click', function() {
-    if(!navbarWrapper.classList.contains('hide-to-left'))
-    {
+closeBtn.addEventListener('click', function () {
+    if (!navbarWrapper.classList.contains('hide-to-left')) {
         navbarWrapper.classList.add('hide-to-left');
-        overlay.style.visibility = 'hidden';
+        overlay.classList.remove('show');
     }
-        
 })
